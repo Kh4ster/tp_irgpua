a49d956a9dd2d9cd0fe4dcd5201af478e5d76b0f
nicolas.blin <nicolas.blin@epita.fr>
2024-08-10 09:35:18 +0200

    add init files and updated the size for tp reduce

---
+++ tp_reduce/src/to_bench.cu
@@ -0,0 +1,57 @@
+#include "to_bench.cuh"
+
+#include "cuda_tools/cuda_error_checking.cuh"
+#include "cuda_tools/host_shared_ptr.cuh"
+
+#include <cuda_profiler_api.h>
+
+
+template <typename T>
+__global__
+void kernel_reduce_baseline(const T* __restrict__ buffer, T* __restrict__ total, int size)
+{
+    const int id = threadIdx.x + blockIdx.x * blockDim.x;
+    if (id < size)
+        atomicAdd(&total[0], buffer[id]);
+}
+
+void baseline_reduce(cuda_tools::host_shared_ptr<int> buffer,
+    cuda_tools::host_shared_ptr<int> total)
+{
+    cudaProfilerStart();
+    cudaFuncSetCacheConfig(kernel_reduce_baseline<int>, cudaFuncCachePreferShared);
+
+    constexpr int blocksize = 64;
+    const int gridsize = (buffer.size_ + blocksize - 1) / blocksize;
+
+	kernel_reduce_baseline<int><<<gridsize, blocksize>>>(buffer.data_, total.data_, buffer.size_);
+
+    cudaDeviceSynchronize();
+    kernel_check_error();
+    
+    cudaProfilerStop();
+}
+
+template <typename T>
+__global__
+void kernel_your_reduce(const T* __restrict__ buffer, T* __restrict__ total, int size)
+{
+    // TODO
+    // ...
+}
+
+void your_reduce(cuda_tools::host_shared_ptr<int> buffer,
+    cuda_tools::host_shared_ptr<int> total)
+{
+    cudaProfilerStart();
+
+    // TODO
+    // ...
+
+	kernel_your_reduce<int><<<1, 1>>>(buffer.data_, total.data_, buffer.size_);
+
+    cudaDeviceSynchronize();
+    kernel_check_error();
+    
+    cudaProfilerStop();
+}
