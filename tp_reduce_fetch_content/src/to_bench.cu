567aebfeaec6eeee585f2b2b1c918870b2c76c40
nicolas.blin <nicolas.blin@epita.fr>
2024-09-16 14:33:30 +0200

    change reduce basline to be single thread

--- tp_reduce_fetch_content/src/to_bench.cu
+++ tp_reduce_fetch_content/src/to_bench.cu
@@ -12,18 +12,14 @@ template <typename T>
 __global__
 void kernel_reduce_baseline(raft::device_span<const T> buffer, raft::device_span<T> total)
 {
-    const int id = threadIdx.x + blockIdx.x * blockDim.x;
-    if (id < buffer.size())
-        atomicAdd(total.data(), buffer[id]);
+    for (int i = 0; i < buffer.size(); ++i)
+        *total.data() += buffer[i];
 }
 
 void baseline_reduce(rmm::device_uvector<int>& buffer,
                      rmm::device_scalar<int>& total)
 {
-    constexpr int blocksize = 64;
-    const int gridsize = (buffer.size() + blocksize - 1) / blocksize;
-
-	kernel_reduce_baseline<int><<<gridsize, blocksize, 0, buffer.stream()>>>(
+	kernel_reduce_baseline<int><<<1, 1, 0, buffer.stream()>>>(
         raft::device_span<int>(buffer.data(), buffer.size()),
         raft::device_span<int>(total.data(), 1));
 
